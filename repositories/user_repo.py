da1ac0d5b49c1f1ef9cbc8b9e50a23c4602676c1
4mh23cs158-pixel <4mh23cs158@gmail.com>
2026-02-18 11:07:51 +0530

    post

--- repositories/user_repo.py
+++ repositories/user_repo.py
@@ -9,7 +9,14 @@ class UserRepo:
     def add_user(self, user: User):
         self.db.add(user)
         self.db.commit()
+        self.db.refresh(user)
         return user
 
     def get_user_by_email(self, email: str):
         return self.db.query(User).filter(User.email == email).first()
+
+    def get_all_users(self):
+        return self.db.query(User).all()
+
+    def get_user_by_id(self, user_id: int):
+        return self.db.query(User).filter(User.id == user_id).first()
--- schemas/user_schemas.py
+++ schemas/user_schemas.py
@@ -1,6 +1,13 @@
 from pydantic import BaseModel
 
-class UserSchema(BaseModel):
+class UserCreate(BaseModel):
     email: str
     password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: str
+
+    class Config:
+        from_attributes = True
 
